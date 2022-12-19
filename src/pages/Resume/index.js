b50bbb921476a7d1b162282997971030c4ef77f5
MoMo R <moran.risk78@gmail.com>
2022-12-19 02:36:43 -0800

    resume updated

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -11,7 +11,39 @@ export default function Resume({currentPage, setCurrentPage}) {
 
   return (
     <div className="resume">
-        <h1>This is my Resume</h1>
+        
+        
+      <div className='contentbox hd'>
+        <h2>Resume</h2>
+        <p>Download my <a href="##">resume</a></p>
+      </div>
+        
+      
+      <div className='contentbox'>
+        <h3>Front-end Proficiencies</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>JQuery</li>
+          <li>Responsive design</li>
+          <li>React</li>
+          <li>Bootstrap</li>
+        </ul>
+      </div>
+
+      <div className='contentbox'>
+        <h3>Back-end Proficiencies</h3>
+        <ul>
+          <li>APIs</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>JQuery</li>
+          <li>MySQL, Sequelize</li>
+        </ul>
+      </div>
+
+
     </div>
   )
 }
